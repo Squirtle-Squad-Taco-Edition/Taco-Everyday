882d110345668874cf40ea80bc118bdb1277a4cd
Jeremiah Hogue <hogue@Jeremiahs-MBP.attlocal.net>
2023-08-28 21:02:01 -0500

    more boilerplate for front end

--- src/components/group.tsx
+++ src/components/group.tsx
@@ -1,12 +1,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { type ReactElement } from 'react'
 import { useNavigate } from 'react-router-dom'
+import lememe from '../assets/lememe.png'
 
 function Group (): ReactElement {
   return (
-    <div className="signInBox">
-      group page
-    </div>
+    <>
+      <div className="navbar">
+        <img className="logoSmall" src={lememe} alt="" />
+        <h1>Welcome to Tacos Everyday</h1>
+      </div>
+      <div className="signInBox">
+        group page
+      </div>
+
+    </>
   )
 }
 
--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -1,36 +1,65 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React, { type ReactElement } from 'react'
+import React, { type ReactElement, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import taco from '../assets/taco.png'
 
 function Signin (): ReactElement {
+  const [showModal, setShowModal] = useState(false)
   const navigate = useNavigate()
   return (
-    <div className="signInBox">
-      <div className="logoContainer">
-        <h1>Not Taco Tuesday</h1>
-        <img className="logo" src={taco} alt="taco" />
-      </div>
-      <input className="signInInput" type="text" placeholder="email or username" />
-      <input className="signInInput" type="text" placeholder="password" />
-      <div className="passwordReset">
-        Forgot Password?
-      </div>
-      <button
-        className="signInSubmit"
-        type="button"
-        onClick={() => {
-          'thingy'
+    <div className="signInPage">
+      <div className="signInBox">
+        <div className="logoContainer">
+          <h1>Not Taco Tuesday</h1>
+          <img className="logo" src={taco} alt="taco" />
+        </div>
+        {showModal
+          ? (
+            <>
+              <input className="signInInput" type="text" placeholder="email" />
+              <input className="signInInput" type="text" placeholder="username" />
+              <input className="signInInput" type="text" placeholder="password" />
+              <input className="signInInput" type="text" placeholder="confirm password" />
+            </>
+            )
+          : (
+            <>
+              <input className="signInInput" type="text" placeholder="email or username" />
+              <input className="signInInput" type="text" placeholder="password" />
 
-          navigate('/group')
-        }}
-      >
-        Submit
-      </button>
-      <div className="signInBottom">
-        dont have an account?
-        {' '}
-        <a>sign up</a>
+            </>
+            )}
+        <div className="passwordReset">
+          Forgot Password?
+        </div>
+        <button
+          className="signInSubmit"
+          type="button"
+          onClick={() => {
+            'thingy'
+
+            navigate('/group')
+          }}
+        >
+          Submit
+        </button>
+        <div className="signInBottom">
+          <div>
+            <hr style={{ margin: 0, padding: 0, marginBottom: '30px', width: '100%' }} />
+            dont have an account?
+            {' '}
+            <button
+              className="signUp"
+              type="button"
+              onClick={() => {
+                showModal ? setShowModal(false) : setShowModal(true)
+              }}
+            >
+              sign up
+
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
