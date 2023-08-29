1dc626deeaeccb56d946f0bf352567d148321fe6
Clayton Stewart <claytons1212@gmail.com>
2023-08-29 11:19:29 -0600

    created database tables

--- server/model/tacoSchema.ts
+++ server/model/tacoSchema.ts
@@ -1,72 +1,58 @@
-const { query } = require('./tacoModel')
-
-console.log(query)
+const { query } = require('./tacoModel');
 
 const createTables = (async (): Promise<void> => {
   try {
     await query(`CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL,
-    email TEXT NOT NULL,
-    favorite_taco TEXT,
-    profile_url TEXT
-)`)
+        user_id SERIAL PRIMARY KEY,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL,
+        email TEXT,
+        favorite_taco TEXT,
+        profile_url TEXT
+    );`);
+
+    await query(`CREATE TABLE groups (
+        group_id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL,
+        creator TEXT NOT NULL,
+        created_at TEXT NOT NULL
+    );`);
+
+    await query(`CREATE TABLE messages (
+        message_id SERIAL PRIMARY KEY,
+        poster_id INTEGER,
+        group_id INTEGER,
+        message TEXT NOT NULL,
+        picture_url TEXT,
+        created_at TEXT NOT NULL,
+        FOREIGN KEY (poster_id) REFERENCES users (user_id),
+        FOREIGN KEY (group_id) REFERENCES groups (group_id)
+    );`);
+
+    await query(`CREATE TABLE tacos (
+        recipe_id SERIAL PRIMARY KEY,
+        taco_url TEXT NOT NULL,
+        created_at TEXT NOT NULL
+    );`);
+
+    await query(`CREATE TABLE user_group (
+        user_id SERIAL,
+        group_id SERIAL,
+        PRIMARY KEY (user_id, group_id),
+        FOREIGN KEY (user_id) REFERENCES users (user_id),
+        FOREIGN KEY (group_id) REFERENCES groups (group_id)
+    );`);
+
+    await query(`CREATE TABLE taco_group (
+      recipe_id SERIAL,
+      group_id SERIAL,
+      PRIMARY KEY (recipe_id, group_id),
+      FOREIGN KEY (recipe_id) REFERENCES tacos (recipe_id),
+      FOREIGN KEY (group_id) REFERENCES groups (group_id)
+  );`);
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-})()
+})();
 
-console.log(createTables)
-/*
-
-* user table for storing basic user info
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL,
-    email TEXT NOT NULL,
-    favorite_taco TEXT,
-    profile_url TEXT
-)
-
-* group table (users can belong to multiple groups)
-CREATE TABLE groups (
-    group_id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    creator TEXT NOT NULL
-)
-
-* message table (stores all messages for all groups)
-CREATE TABLE messages (
-    message_id SERIAL PRIMARY KEY,
-    poster_id SERIAL FOREIGN KEY,
-    group_id SERIAL FOREIGN KEY,
-    message TEXT NOT NULL,
-    picture_url TEXT,
-    created_at TEXT NOT NULL
-)
-
-* taco table (stores all recipies)
-CREATE TABLE tacos (
-    recipe_id SERIAL PRIMARY KEY,
-    taco_url TEXT NOT NULL,
-    taco_ingredients TEXT NOT NULL,
-    taco_instructions TEXT NOT NULL
-)
-
-* join table between user and group table
-CREATE TABLE user_group (
-  user_id SERIAL PRIMARY KEY,
-  group_id SERIAL PRIMARY KEY,
-  creator TEXT NOT NULL
-)
-
-* join table between recipie and group table
-CREATE TABLE taco_group (
-  recipe_id SERIAL PRIMARY KEY,
-  group_id SERIAL PRIMARY KEY,
-  created_at TEXT NOT NULL
-)
-
-*/
+console.log(createTables);
--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import express from 'express'
-import type { Request, Response, NextFunction } from 'express'
-import type { ServerError } from '../types/types'
+import express from 'express';
+import type { Request, Response, NextFunction } from 'express';
+import type { ServerError } from '../types/types';
 // import { tacoController } from './controllers/tacoController'
+const PORT = 3030;
 
-const PORT = 3030
-
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 // general endpoint for routes
 
@@ -15,22 +14,22 @@ app.use(express.json())
 
 // error handler for bad routes/requests to backend
 app.use((req, res) => {
-  res.sendStatus(404)
-})
+  res.sendStatus(404);
+});
 
 // global error handler for all middleware and routes
 app.use((err: ServerError, req: Request, res: Response, next: NextFunction) => {
   const defaultErr = {
     log: 'Error caught in global handler',
     status: 500,
-    message: { err: 'An error occurred' }
-  }
-  const errorObj = { ...defaultErr, ...err }
-  console.log(errorObj.log)
-  console.log(err)
-  return res.status(errorObj.status).json(errorObj.message)
-})
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = { ...defaultErr, ...err };
+  console.log(errorObj.log);
+  console.log(err);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
-})
-export default app
+  console.log(`App listening on port ${PORT}`);
+});
+export default app;
