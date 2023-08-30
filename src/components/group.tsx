50485226ed08a79cef3902d60eeac0131fcc5ed4
Jeremiah Hogue <hogue@Jeremiahs-MBP.attlocal.net>
2023-08-29 21:39:23 -0500

    starting to work on tacos

--- src/components/group.tsx
+++ src/components/group.tsx
@@ -2,7 +2,8 @@
 import React, { useContext, useState, useRef, type ReactElement, useEffect } from 'react'
 import { io } from 'socket.io-client'
 import { v4 as uuidv4 } from 'uuid'
-import CustomScroller from 'react-custom-scroller'
+// import CustomScroller from 'react-custom-scroller'
+import { type TacoObj } from '../../types/types'
 import { GlobalContext } from './Context'
 import Navbar from './Navbar'
 
@@ -10,6 +11,7 @@ const tempArr = ['i love tacos', 'i love tacos', 'i love tacos', 'i love tacos',
 function Group (): ReactElement {
   const [message, setMessage] = useState('')
   const [newMessage, setNewMessage] = useState('')
+  const [taco, setTaco] = useState<TacoObj>()
   const [msgArr, setMsgArr] = useState<string[]>(tempArr)
   const messagesEndRef = useRef<null | HTMLDivElement>(null)
   const socket = io('http://127.0.0.1:3030', { transports: ['websocket'] })
@@ -21,6 +23,19 @@ function Group (): ReactElement {
   const scrollToBottom = (): void => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth', block: 'start' })
   }
+  async function getTaco (): Promise<void> {
+    try {
+      const result = await fetch('api/taco/new/3') // TODO dynamically pull group id
+      const data = await result.json()
+      setTaco(data)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    if (taco === undefined) void getTaco()
+  }, [])
   useEffect(() => {
     const arr: string[] = [...msgArr]
     if (message !== '' || newMessage !== '') arr.push(newMessage)
@@ -30,9 +45,31 @@ function Group (): ReactElement {
   useEffect(() => {
     scrollToBottom()
   }, [msgArr])
+  // ${Math.round(taco.totalNutrients[nutrient].quantity)}${taco.totalNutrients[nutrient].unit}
   return (
     <>
       <Navbar />
+      {taco !== undefined &&
+      (
+      <div className="tacoBox">
+        <h3>{taco.label}</h3>
+        <img className="logo" src={taco.imageurl} alt="main dish" />
+        <div className="desc" style={{ fontSize: 'small' }}>{taco.description}</div>
+        <div className="infoBox">
+          <div className="infoSmall">
+            <a className="recipeLink" href={taco.url}>Recipe Post</a>
+            <p style={{ fontSize: 'small' }}>{`Servings: ${taco.servings}`}</p>
+            <p style={{ fontSize: 'small' }}>{`Time to Cook: ${taco.totalTime} minutes`}</p>
+          </div>
+          <div className="infoSmall">
+            <p style={{ fontSize: 'small' }}>{`Calories: ${Math.round(taco.calories)}`}</p>
+            {Object.entries(taco.totalNutrients).map(([nutrient, obj]): ReactElement => (
+              <p key={uuidv4()} style={{ fontSize: 'small' }}>{`${nutrient}: ${Math.round(obj.quantity)}${obj.unit}`}</p>
+            ))}
+          </div>
+        </div>
+      </div>
+      )}
       <div className="groupBox">
         <h3>
           Taco bout it
--- types/types.ts
+++ types/types.ts
@@ -1,9 +1,26 @@
-import { type IncomingMessage } from 'http'
-import type { RequestHandler } from 'express'
-import { type Dispatch, type SetStateAction } from 'react'
+// import { type IncomingMessage } from 'http'
+// import type { RequestHandler } from 'express'
+// import { type Dispatch, type SetStateAction } from 'react'
 
 export interface ServerError {
-    err: '400'
-  }
+  err: '400'
+}
 
-  
+export interface TacoObj {
+  [key: string]: any
+  label: string
+  description: string
+  url: string
+  imageurl: string
+  calories: number
+  totalNutrients: NutrientObj[]
+  totalTime: number
+  servings: number
+}
+
+export interface NutrientObj {
+  [key: string]: any
+  label: string
+  quantity: number
+  unit: string
+}
